d60912aeeae74f83439929ce13323ef3e56266db
William Anderson <willjatlas@googlemail.com>
2020-11-06 16:25:43 +0000

    function to get cash from admin complete

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,6 +1,12 @@
 # Codeclan week 1 - Day 5 - Weekend Homework
 
-# Return the 
-def get_pet_shop_name(input_list):
-    return input_list["name"]
+# Return the value from the "name" key in the cc_pet_shop
+def get_pet_shop_name(input_dict):
+    return input_dict["name"]
+
+
+# Returns the value from the "total_cash" key in the admin dict of cc_pet_shop
+def get_total_cash(input_dict):
+    return input_dict["admin"]["total_cash"]
+
 
